fd10818f7fbb5aad3765c478ace7c5b521c25335
Caveh Jalali <caveh@chromium.org>
2022-01-05 16:28:53 -0800

    ps8751: Add flash ops initializer
    The parade ps8xxx TCPC firmware flashing code was recently
    refactored. Support code for some members of the ps8xxx family was not
    updated to include the new flash ops initialization call. This updates
    the ps8705, ps8751, ps8755, ps8805 support code to properly initialize
    the flash ops.
    
    BRANCH=none
    BUG=b:200695249,b:208556654
    TEST=verified on herobrine by shchen@
    
    Change-Id: I086559f6ddd16307e43b5025bf01991443bd81ca
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3368969
    Reviewed-by: Shelley Chen <shchen@chromium.org>
    Commit-Queue: Shelley Chen <shchen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6354650
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Nick Vaccaro <nvaccaro@google.com>
    Tested-by: Nick Vaccaro <nvaccaro@google.com>

--- src/drivers/ec/ps8751/ps8751.c
+++ src/drivers/ec/ps8751/ps8751.c
@@ -1684,6 +1684,14 @@ static const VbootAuxfwOps ps8815_a2_fw_ops = {
 	.update_image = ps8751_update_image,
 };
 
+static void ps8751_init_flash_ops(Ps8751 *me)
+{
+	if (CONFIG(DRIVER_EC_PS8751_FLASH_WINDOW))
+		ps8751_init_flash_window_ops(me);
+	else
+		ps8751_init_flash_fifo_ops(me);
+}
+
 Ps8751 *new_ps8751(CrosECTunnelI2c *bus, int ec_pd_id)
 {
 	Ps8751 *me = xzalloc(sizeof(*me));
@@ -1693,6 +1701,7 @@ Ps8751 *new_ps8751(CrosECTunnelI2c *bus, int ec_pd_id)
 	me->fw_ops = ps8751_fw_ops;
 	me->chip_type = CHIP_PS8751;
 	snprintf(me->chip_name, sizeof(me->chip_name), "ps8751.%d", ec_pd_id);
+	ps8751_init_flash_ops(me);
 
 	return me;
 }
@@ -1706,6 +1715,7 @@ Ps8751 *new_ps8751_canary(CrosECTunnelI2c *bus, int ec_pd_id)
 	me->fw_ops = ps8751_fw_canary_ops;
 	me->chip_type = CHIP_PS8751;
 	snprintf(me->chip_name, sizeof(me->chip_name), "ps8751.%d", ec_pd_id);
+	ps8751_init_flash_ops(me);
 
 	return me;
 }
@@ -1719,6 +1729,7 @@ Ps8751 *new_ps8755(CrosECTunnelI2c *bus, int ec_pd_id)
 	me->fw_ops = ps8755_fw_ops;
 	me->chip_type = CHIP_PS8755;
 	snprintf(me->chip_name, sizeof(me->chip_name), "ps8755.%d", ec_pd_id);
+	ps8751_init_flash_ops(me);
 
 	return me;
 }
@@ -1733,6 +1744,7 @@ Ps8751 *new_ps8705(CrosECTunnelI2c *bus, int ec_pd_id,
 	me->fw_ops = *fw_ops;
 	me->chip_type = CHIP_PS8705;
 	snprintf(me->chip_name, sizeof(me->chip_name), "ps8705.%d", ec_pd_id);
+	ps8751_init_flash_ops(me);
 
 	return me;
 }
@@ -1756,6 +1768,7 @@ Ps8751 *new_ps8805(CrosECTunnelI2c *bus, int ec_pd_id)
 	me->fw_ops = ps8805_fw_ops;
 	me->chip_type = CHIP_PS8805;
 	snprintf(me->chip_name, sizeof(me->chip_name), "ps8805.%d", ec_pd_id);
+	ps8751_init_flash_ops(me);
 
 	return me;
 }
@@ -1770,11 +1783,7 @@ static Ps8751 *new_ps8815(CrosECTunnelI2c *bus, int ec_pd_id,
 	me->fw_ops = *fw_ops;
 	me->chip_type = CHIP_PS8815;
 	snprintf(me->chip_name, sizeof(me->chip_name), "ps8815.%d", ec_pd_id);
-
-	if (CONFIG(DRIVER_EC_PS8751_FLASH_WINDOW))
-		ps8751_init_flash_window_ops(me);
-	else
-		ps8751_init_flash_fifo_ops(me);
+	ps8751_init_flash_ops(me);
 
 	return me;
 }
