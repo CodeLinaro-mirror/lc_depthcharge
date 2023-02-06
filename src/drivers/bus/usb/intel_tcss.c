59459734193668618337f5f42cb4fdb47408943a
Subrata Banik <subratabanik@google.com>
2023-02-06 19:16:21 +0530

    CHROMIUM: drivers/../../intel_tcss: Avoid sending PMC IPC repetitively
    This patch fixes a persisting issue on the Intel latest SoC platform
    (starting with Tiger Lake SoC) where sending PMC IPC (for example:
    CONN IPC) consecutively results in failure.
    
    As per Intel PMC team, "Connection to an already connected port would
    be treated as an error scenario".
    
    Example:
    
      sending connect PMC IPC on the TCSS ports repeatedly results in failure after the first attempt is successful.
      update_all_tcss_ports_states: port C0 state: usb enable 1 mux conn 0, usb2 1, usb3 1
      update_all_tcss_ports_states: port C0 req: usage 0 usb3 1 usb2 1 ufp 0 ori_hsl 1 ori_sbu 1 dbg_acc 1
      pmc_check_ipc_sts: STS_BUSY done after 863 us
      send_conn_disc_msg: pmc_send_cmd succeeded
    
      update_all_tcss_ports_states: port C1 state: usb enable 1 mux conn 0, usb2 3, usb3 3
      update_all_tcss_ports_states: port C1 req: usage 0 usb3 3 usb2 3 ufp 0 ori_hsl 1 ori_sbu 1 dbg_acc 0
      pmc_check_ipc_sts: STS_BUSY done after 962 us
      send_conn_disc_msg: pmc_send_cmd succeeded
    
      update_all_tcss_ports_states: port C0 state: usb enable 1 mux conn 1, usb2 1, usb3 1
      update_all_tcss_ports_states: port C0 req: usage 0 usb3 1 usb2 1 ufp 0 ori_hsl 1 ori_sbu 1 dbg_acc 1
      pmc_check_ipc_sts: STS_BUSY done after 155 us
      send_conn_disc_msg: pmc_send_cmd status: fatal
    
    This patch creates a custom data structure to keep track of TCSS
    Port PMC IPC status. If PMC IPC to a given port is already
    successful then avoid resending the IPC again (this would help to
    avoid running into `fatal` error. Only update the data structure
    with PMC IPC (CONN and DISC) status upon response received from
    the PMC IPC command.
    
    Test Scenarios:
    1. Servov4.1 attached to the Port 0 and USB-C pen-drive at Port 1.
    2. Port 0 empty and booting from USB-C over Port 1.
    3. Port 1 empty and booting from USB-C over Port 0.
    4. Both USB-C ports are empty and the system is booting into recovery
       mode. Connected USB-C pen drive at either USB port to boot.
    5. Connect USB-C with invalid partition and results in failure to
       boot from USB-C. Remove the USB-C and connect another USB-C with a
       valid OS.
    
    BUG=b:263770936
    TEST=No fatal error seen at depthcharge while booting google/rex.
    
    Change-Id: I22250364341a574d9a13d31f8f699d975aab9279
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4193752
    Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: John Zhao <john.zhao@intel.corp-partner.google.com>
    Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6365380
    Commit-Queue: Kapil Porwal <kapilporwal@chromium.org>
    Reviewed-by: Pranava Y N <pranavayn@google.com>

--- src/drivers/bus/usb/intel_tcss.c
+++ src/drivers/bus/usb/intel_tcss.c
@@ -34,6 +34,14 @@
 #define TCSS_CONN_REQ_RES		0
 #define TCSS_DISC_REQ_RES		1
 
+enum typec_port_index {
+	TYPE_C_PORT_0,
+	TYPE_C_PORT_1,
+	TYPE_C_PORT_2,
+	TYPE_C_PORT_3,
+	MAX_TYPE_C_PORTS,
+};
+
 /*
  * Polling the MUX significantly slows down depthcharge when something
  * is connected, so dampen the polls.
@@ -163,6 +171,7 @@ static int get_typec_hsl_sbu_orientation(bool mux_is_inverted, enum type_c_orien
  */
 static int update_all_tcss_ports_states(void)
 {
+	static bool tcss_conn_req_res[MAX_TYPE_C_PORTS] = { 0 };
 	const struct type_c_port_info *tcss_port_info;
 	const struct type_c_info *type_c_info;
 	struct pmc_ipc_buffer req = { 0 };
@@ -190,6 +199,7 @@ static int update_all_tcss_ports_states(void)
 		}
 
 		usb_enabled = !!(mux_state & USB_PD_MUX_USB_ENABLED);
+		usb2 = tcss_port_info[ec_port].usb2_port_number;
 		usb3 = tcss_port_info[ec_port].usb3_port_number;
 
 		/*
@@ -204,7 +214,16 @@ static int update_all_tcss_ports_states(void)
 			continue;
 		}
 
-		usb2 = tcss_port_info[ec_port].usb2_port_number;
+		/*
+		 * b/263770936: As per Intel PMC team recommendation
+		 * "Connection (CONN) PMC IPC to an already connected port would be treated
+		 * as an error scenario". Hence, we would need to avoid the back to
+		 * back 0xa7 IPC with CONN request.
+		 */
+		if (tcss_conn_req_res[ec_port] == true && usb_enabled) {
+			continue;
+		}
+
 		debug("port C%d state: usb enable %d mux conn %d, usb2 %u, "
 				"usb3 %u\n", ec_port, usb_enabled,
 				is_port_connected(usb3-1), usb2, usb3);
@@ -251,6 +270,10 @@ static int update_all_tcss_ports_states(void)
 		      GET_TCSS_CD_FIELD(ACC, cmd));
 
 		r = send_conn_disc_msg(&req, &res);
+		if (!r) {
+			if (GET_TCSS_CD_FIELD(USAGE, cmd) == TCSS_CONN_REQ_RES)
+				tcss_conn_req_res[ec_port] = true;
+		}
 	}
 
 	return r;
