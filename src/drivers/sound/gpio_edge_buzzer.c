ab765150ddf5fe13b17a63f2d79c64bf345b68bb
Dengwu Yu <yudengwu@huaqin.corp-partner.google.com>
2024-08-28 19:18:51 +0800

    drivers/sound/gpio_sound_buzzer: Use beep frequency
    Determine the beep period using the input frequency and
    limit the beep frequency to 2.7KHz.
    
    BUG=b:346956771
    BRANCH=None
    TEST=emerge-brox depthcharge
    TEST=verify beep function through deploy in depthcharge successfully
    
    Change-Id: Iecc41a494e886adcc0a644dfc685e28bc5833b29
    Signed-off-by: Dengwu Yu <yudengwu@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5822799
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    (cherry picked from commit 54c1fdd9952f32f2b7e7c1c2e8429e3f19842cf4)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6703033
    Tested-by: Zheng Li <lizheng@huaqin.corp-partner.google.com>
    Reviewed-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Commit-Queue: Ashley Shen <shenashley@google.com>
    Reviewed-by: David Cross <davidmcross@google.com>

--- src/drivers/sound/gpio_edge_buzzer.c
+++ src/drivers/sound/gpio_edge_buzzer.c
@@ -17,20 +17,28 @@
 #include "base/container_of.h"
 #include "drivers/sound/gpio_edge_buzzer.h"
 
+static const uint32_t USEC_PER_SEC = 1000000;
+static const uint32_t MAX_BUZZER_FREQUENCY_HZ = 2700;
+static const uint32_t MIN_BUZZER_PERIOD_US = (USEC_PER_SEC / MAX_BUZZER_FREQUENCY_HZ);
+
 static int buzzer_play(SoundOps *me, uint32_t msec, uint32_t frequency)
 {
 	GpioEdgeBuzzer *buzzer = container_of(me, GpioEdgeBuzzer, ops);
 	u64 start;
+	int period_us;
 	int i = 0;
 
 	if (!frequency)
 		return -1;
 
-	/* Using frequency ~2.7kHz, which is peak loudness */
+	/* Limiting frequency to 2.7kHz which is peak loudness */
+	period_us = USEC_PER_SEC / frequency;
+	period_us = (period_us < MIN_BUZZER_PERIOD_US) ? MIN_BUZZER_PERIOD_US : period_us;
+
 	start = timer_us(0);
 	while (timer_us(start)/1000 < msec) {
 		buzzer->gpio->set(buzzer->gpio, ++i & 1);
-		udelay(185);
+		udelay(period_us/2);
 	}
 	buzzer->gpio->set(buzzer->gpio, 0);
 
