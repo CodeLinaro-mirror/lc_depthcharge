da850f4729cbf142a0090341a242e7100aa26afd
Tomasz Michalec <tmichalec@google.com-copybot-pick>
2025-01-08 17:54:13 +0100

    fastboot: Print IP address on the EC console Android Bringup: See http://go/android-fw-sync
    It is common to use AP FW without serial output. To make it easier to
    obtain IP address from DUTs that are remote, print IP address on the EC
    console too. Message is printed using existing host command.
    
    BRANCH=main
    BUG=b:388507818
    TEST=start fastboot and look for a message on the EC console
    
    (cherry picked from commit baf1c0e9bddaa7ca57fe1f642eb14c2a5348e2d2)
    
    Change-Id: I049199f952db450e27f56382ee830ee908109e20
    Original-Signed-off-by: Tomasz Michalec <tmichalec@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6157417
    Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
    GitOrigin-RevId: baf1c0e9bddaa7ca57fe1f642eb14c2a5348e2d2
    Cr-Build-Id: 8725736108671290609
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8725736108671290609
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6175247
    Bot-Commit: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    (cherry picked from commit a8199260afb61b919f9040e484f9f5b41d23b556)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6210864
    Reviewed-by: Konrad Adamczyk <konrada@google.com>
    Commit-Queue: Tomasz Michalec <tmichalec@google.com>
    Tested-by: Tomasz Michalec <tmichalec@google.com>

--- src/drivers/ec/cros/ec.h
+++ src/drivers/ec/cros/ec.h
@@ -509,4 +509,13 @@ int cros_ec_i2c_passthru_protect(int i2c_port);
  */
 int cros_ec_i2c_passthru_protect_status(int i2c_port, int *status);
 
+/**
+ * Print message on the EC console.
+ *
+ * @param fmt		Format string
+ * @param ...		printf like arguments for format string
+ * @return 0 if ok, negative on error
+ */
+int cros_ec_print(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+
 #endif /* __DRIVERS_EC_CROS_EC_H__ */
