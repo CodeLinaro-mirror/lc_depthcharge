cd71cf0ad66e4e68960400080c08ab6ab8c1a8e1
Kornel Dulęba <korneld@google.com-copybot-pick>
2025-02-18 16:12:55 +0000

    fastboot: Add support for "current-slot" variable Android Bringup: See http://go/android-fw-sync
    It returns the current a.k.a. active a/b slot the OS will boot from.
    To avoid code duplication re-use "GptNextKernelEntry" from vboot to find
    the active kernel partition. The same function is used by depthcharge
    during boot. Note that this logic only works when android is installed
    on internal storage, if the device is flashed with ChromeOS this command
    will return an error.
    
    BRANCH=none
    BUG=b:397322409
    TEST=fastboot -s tcp:${dut_ip}:5554 getvar current-slot && \
         fastboot -s tcp:${dut_ip}:5554 set_active b && \
         fastboot -s tcp:${dut_ip}:5554 getvar current-slot && \
         fastboot -s tcp:${dut_ip}:5554 set_active a && \
         fastboot -s tcp:${dut_ip}:5554 getvar current-slot
    
    (cherry picked from commit c12e941f6ff5a7bb1b2f9315209e20c4d7d003ed)
    
    Cq-Depend: chromium:6301800
    Original-Signed-off-by: Kornel Dulęba <korneld@google.com>
    Change-Id: Ib645f2693fbe7b454ad681e23d643ed431dca239
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6300494
    Original-Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    GitOrigin-RevId: c12e941f6ff5a7bb1b2f9315209e20c4d7d003ed
    Cr-Build-Id: 8721704542213916417
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8721704542213916417
    Copybot-Job-Name: android-main-fw-brya-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6301835
    Reviewed-by: Konrad Adamczyk <konrada@google.com>
    Tested-by: Kornel Dulęba <korneld@google.com>
    Commit-Queue: Kornel Dulęba <korneld@google.com>

--- src/fastboot/vars.c
+++ src/fastboot/vars.c
@@ -32,6 +32,7 @@
 		.name = _name, .has_args = false, .var = _var                  \
 	}
 static fastboot_getvar_info_t fastboot_vars[] = {
+	VAR_NO_ARGS("current-slot", VAR_CURRENT_SLOT),
 	VAR_NO_ARGS("max-download-size", VAR_DOWNLOAD_SIZE),
 	VAR_NO_ARGS("is-userspace", VAR_IS_USERSPACE),
 	VAR_ARGS("partition-size", ':', VAR_PARTITION_SIZE),
@@ -119,8 +120,31 @@ fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 					 size_t arg_len, char *outbuf,
 					 size_t *outbuf_len)
 {
+	struct fastboot_disk disk;
 	size_t used_len = 0;
+	char *suffix;
+	int ret;
+
 	switch (var) {
+	case VAR_CURRENT_SLOT:
+		uint64_t start_sector, size;
+		if (!fastboot_disk_init(&disk))
+			return STATE_DISK_ERROR;
+
+		ret = GptNextKernelEntry(disk.gpt, &start_sector, &size);
+		if (ret != GPT_SUCCESS) {
+			fastboot_disk_destroy(&disk);
+			return STATE_DISK_ERROR;
+		}
+
+		ret = GptGetActiveKernelPartitionSuffix(disk.gpt, &suffix);
+		fastboot_disk_destroy(&disk);
+		if (ret != GPT_SUCCESS)
+			return STATE_DISK_ERROR;
+
+		used_len = snprintf(outbuf, *outbuf_len, "%s", suffix);
+		free(suffix);
+		break;
 	case VAR_DOWNLOAD_SIZE:
 		used_len = snprintf(outbuf, *outbuf_len, "%llu",
 				    FASTBOOT_MAX_DOWNLOAD_SIZE);
@@ -129,7 +153,6 @@ fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 		used_len = snprintf(outbuf, *outbuf_len, "no");
 		break;
 	case VAR_PARTITION_SIZE: {
-		struct fastboot_disk disk;
 		GptEntry *part = NULL;
 
 		if (!fastboot_disk_init(&disk))
@@ -172,7 +195,6 @@ fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 		break;
 	}
 	case VAR_SLOT_COUNT: {
-		struct fastboot_disk disk;
 		if (!fastboot_disk_init(&disk))
 			return STATE_DISK_ERROR;
 		used_len = snprintf(outbuf, *outbuf_len, "%d",
