712085ab9a518f22ec31972896c0a3fbdd6d33e3
Eran Mitrani <mitrani@google.com-copybot-pick>
2025-04-01 10:45:34 -0700

    fastboot: Add support for "getvar slot-retry-count" command Android Bringup: See http://go/android-fw-sync
    This CL adds a variable "slot-retry-count" to getvar. It returns number
    of retry attempts.
    
    BUG=b:395616658
    TEST=fastboot -s tcp:{dut_ip}:5554 getvar slot-retry-count:a
    fastboot -s tcp:{dut_ip}:5554 getvar slot-retry-count:b
    
    (cherry picked from commit 9da33411937062afcd753a5b46c1043f60750def)
    
    Change-Id: Idc2c62a7b06498413555853c62e1aecbf12af5e9
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6422058
    Original-Tested-by: Eran Mitrani <mitrani@google.com>
    Original-Commit-Queue: Jakub "Kuba" Czapiga <czapiga@google.com>
    Original-Auto-Submit: Eran Mitrani <mitrani@google.com>
    Original-Reviewed-by: Jakub "Kuba" Czapiga <czapiga@google.com>
    GitOrigin-RevId: 9da33411937062afcd753a5b46c1043f60750def
    Cr-Build-Id: 8718216560061166241
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8718216560061166241
    Copybot-Job-Name: android-main-fw-corsola-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6439579
    Bot-Commit: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

--- src/fastboot/vars.c
+++ src/fastboot/vars.c
@@ -44,6 +44,7 @@ static fastboot_getvar_info_t fastboot_vars[] = {
 	VAR_ARGS("has-slot", ':', VAR_HAS_SLOT),
 	VAR_NO_ARGS("slot-suffixes", VAR_SLOT_SUFFIXES),
 	VAR_ARGS("slot-successful", ':', VAR_SLOT_SUCCESSFUL),
+	VAR_ARGS("slot-retry-count", ':', VAR_SLOT_RETRY_COUNT),
 	{.name = NULL},
 };
 
@@ -262,6 +263,29 @@ fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 		fastboot_disk_destroy(&disk);
 		break;
 	}
+	case VAR_SLOT_RETRY_COUNT: {
+		if (!fastboot_disk_init(&disk))
+			return STATE_DISK_ERROR;
+
+		if (strlen(arg) != 1 || !isalpha(arg[0])) {
+			fastboot_disk_destroy(&disk);
+			return STATE_UNKNOWN_VAR;
+		}
+
+		char slot = tolower(arg[0]);
+
+		GptEntry *e = fastboot_get_kernel_for_slot(&disk, slot);
+		if (e == NULL) {
+			fastboot_disk_destroy(&disk);
+			return STATE_UNKNOWN_VAR;
+		}
+
+		used_len = snprintf(outbuf, *outbuf_len, "%d", GetEntryTries(e));
+
+		fastboot_disk_destroy(&disk);
+		break;
+	}
+
 	default:
 		return STATE_UNKNOWN_VAR;
 	}
