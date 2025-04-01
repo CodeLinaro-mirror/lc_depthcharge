7c20d11894cd6689bea965f651306d6a4305a5e7
Eran Mitrani <mitrani@google.com>
2025-04-01 13:40:01 -0700

    fastboot: make slot name match partition suffix
    Current implementation of fastboot_cmd_oem_get_kernels and
    fastboot_cmd_set_active map slot suffix to internal slot number.
    I.e. first valid kernel partition in GPT table is represented by
    slot "a",no matter what is the real suffix of that partition.
    This CL corrects it such name matches partition suffix, e.g. boot_b is
    always slot b
    
    BUG=b:395616658
    TEST=See Below
    fastboot -s tcp:{dut_ip}:5554 oem get-kernels
    (bootloader) a:boot_a:prio=2
    (bootloader) b:boot_b:prio=0
    fastboot -s tcp:172.16.243.164:5554 set_active b
    Setting current slot to 'b'                        OKAY [  0.023s]
    fastboot -s tcp:{dut_ip}:5554 oem get-kernels
    (bootloader) a:boot_a:prio=0
    (bootloader) b:boot_b:prio=2
    
    Change-Id: I5d627e7fcd57f1d4d2d88f03a76a75ac2165af6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6422059
    Commit-Queue: Eran Mitrani <mitrani@google.com>
    Tested-by: Eran Mitrani <mitrani@google.com>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Auto-Submit: Eran Mitrani <mitrani@google.com>
    (cherry picked from commit 4e9d76f421802c134323dad8c2f119bb524166df)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6559341
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: Yidi Lin <yidilin@google.com>
    Commit-Queue: Yidi Lin <yidilin@google.com>

--- src/fastboot/disk.c
+++ src/fastboot/disk.c
@@ -331,18 +331,17 @@ int fastboot_get_slot_count(struct fastboot_disk *disk)
 
 struct find_slot_ctx {
 	GptEntry *target_entry;
-	int count;
-	int desired_slot;
+	char desired_slot;
 };
 static bool find_slot_callback(void *ctx, int index, GptEntry *e,
 			       char *partition_name)
 {
-	if (get_slot_for_partition_name(e, partition_name) == 0)
+	char slot = get_slot_for_partition_name(e, partition_name);
+	if (slot == 0)
 		return false;
 
 	struct find_slot_ctx *fs = (struct find_slot_ctx *)ctx;
-	fs->count++;
-	if (fs->count == fs->desired_slot) {
+	if (slot == fs->desired_slot) {
 		fs->target_entry = e;
 		return true;
 	}
@@ -358,8 +357,7 @@ GptEntry *fastboot_get_kernel_for_slot(struct fastboot_disk *disk, char slot)
 	}
 	struct find_slot_ctx ctx = {
 		.target_entry = NULL,
-		.count = 0,
-		.desired_slot = 1 + (slot - 'a'),
+		.desired_slot = slot,
 	};
 	if (fastboot_disk_foreach_partition(disk, find_slot_callback, &ctx)) {
 		return ctx.target_entry;
