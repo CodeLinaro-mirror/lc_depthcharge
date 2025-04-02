7da0d4a7688dbab36ad51d0ae48da5f40df432bf
Masa Nakura <nakura@google.com>
2025-04-02 02:44:35 +0000

    vboot/ui/lvgl: Disable unnecessary LVGL configs
    lv_conf.h has many definitions to disable/enable build for most LVGL
    features. Disable unnecessary LVGL configurations to reduce code size.
    This reduces depthcharge.elf size by 47KB (46689 bytes).
    
    BRANCH=firmware-lvgl-16187.B
    BUG=b:386913035
    TEST=USE=lvgl corsola-emerge depthcharge chromeos-bootimage
    
    Change-Id: Ibbfa45db9c6f47d92424bb85c43ed4e118b8d12d
    Signed-off-by: Masa Nakura <nakura@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6421540
    Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- src/vboot/ui/lvgl/lv_conf.h
+++ src/vboot/ui/lvgl/lv_conf.h
@@ -180,10 +180,10 @@
 	#define LV_DRAW_SW_SUPPORT_RGB888	   1
 	#define LV_DRAW_SW_SUPPORT_XRGB8888	 1
 	#define LV_DRAW_SW_SUPPORT_ARGB8888	 1
-	#define LV_DRAW_SW_SUPPORT_L8		   1
-	#define LV_DRAW_SW_SUPPORT_AL88		 1
-	#define LV_DRAW_SW_SUPPORT_A8		   1
-	#define LV_DRAW_SW_SUPPORT_I1		   1
+	#define LV_DRAW_SW_SUPPORT_L8		   0
+	#define LV_DRAW_SW_SUPPORT_AL88		 0
+	#define LV_DRAW_SW_SUPPORT_A8		   0
+	#define LV_DRAW_SW_SUPPORT_I1		   0
 
 	/** Set number of draw units.
 	 *  - > 1 requires operating system to be enabled in `LV_USE_OS`.
@@ -581,10 +581,10 @@
 
 /* Montserrat fonts with ASCII range and some symbols using bpp = 4
  * https://fonts.google.com/specimen/Montserrat */
-#define LV_FONT_MONTSERRAT_8  0
+#define LV_FONT_MONTSERRAT_8  1
 #define LV_FONT_MONTSERRAT_10 0
 #define LV_FONT_MONTSERRAT_12 0
-#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_14 0
 #define LV_FONT_MONTSERRAT_16 0
 #define LV_FONT_MONTSERRAT_18 0
 #define LV_FONT_MONTSERRAT_20 0
@@ -628,7 +628,7 @@
 #define LV_FONT_CUSTOM_DECLARE
 
 /** Always set a default font */
-#define LV_FONT_DEFAULT (&lv_font_montserrat_14)
+#define LV_FONT_DEFAULT (&lv_font_montserrat_8)
 
 /** Enable handling large font and/or fonts with a lot of characters.
  *  The limit depends on the font size, font face and bpp.
@@ -705,17 +705,17 @@
  * */
 #define LV_WIDGETS_HAS_DEFAULT_VALUE  1
 
-#define LV_USE_ANIMIMG	1
+#define LV_USE_ANIMIMG	0
 
-#define LV_USE_ARC		1
+#define LV_USE_ARC		0
 
-#define LV_USE_BAR		1
+#define LV_USE_BAR		0
 
 #define LV_USE_BUTTON		1
 
-#define LV_USE_BUTTONMATRIX  1
+#define LV_USE_BUTTONMATRIX  0
 
-#define LV_USE_CALENDAR   1
+#define LV_USE_CALENDAR   0
 #if LV_USE_CALENDAR
 	#define LV_CALENDAR_WEEK_STARTS_MONDAY 0
 	#if LV_CALENDAR_WEEK_STARTS_MONDAY
@@ -732,19 +732,19 @@
 	#define LV_USE_CALENDAR_CHINESE 0
 #endif  /*LV_USE_CALENDAR*/
 
-#define LV_USE_CANVAS	 1
+#define LV_USE_CANVAS	 0
 
-#define LV_USE_CHART	  1
+#define LV_USE_CHART	  0
 
-#define LV_USE_CHECKBOX   1
+#define LV_USE_CHECKBOX   0
 
-#define LV_USE_DROPDOWN   1   /**< Requires: lv_label */
+#define LV_USE_DROPDOWN   0   /**< Requires: lv_label */
 
-#define LV_USE_IMAGE	  1   /**< Requires: lv_label */
+#define LV_USE_IMAGE	  0   /**< Requires: lv_label */
 
-#define LV_USE_IMAGEBUTTON	 1
+#define LV_USE_IMAGEBUTTON	 0
 
-#define LV_USE_KEYBOARD   1
+#define LV_USE_KEYBOARD   0
 
 #define LV_USE_LABEL	  1
 #if LV_USE_LABEL
@@ -756,48 +756,48 @@
 	#define LV_LABEL_WAIT_CHAR_COUNT 3
 #endif
 
-#define LV_USE_LED		1
+#define LV_USE_LED		0
 
-#define LV_USE_LINE	   1
+#define LV_USE_LINE	 0
 
-#define LV_USE_LIST	   1
+#define LV_USE_LIST	 0
 
 #define LV_USE_LOTTIE	 0  /**< Requires: lv_canvas, thorvg */
 
-#define LV_USE_MENU	   1
+#define LV_USE_MENU	 0
 
-#define LV_USE_MSGBOX	 1
+#define LV_USE_MSGBOX	 0
 
-#define LV_USE_ROLLER	 1   /**< Requires: lv_label */
+#define LV_USE_ROLLER	 0   /**< Requires: lv_label */
 
-#define LV_USE_SCALE	  1
+#define LV_USE_SCALE	  0
 
-#define LV_USE_SLIDER	 1   /**< Requires: lv_bar */
+#define LV_USE_SLIDER	 0   /**< Requires: lv_bar */
 
-#define LV_USE_SPAN	   1
+#define LV_USE_SPAN	   0
 #if LV_USE_SPAN
 	/** A line of text can contain this maximum number of span descriptors. */
 	#define LV_SPAN_SNIPPET_STACK_SIZE 64
 #endif
 
-#define LV_USE_SPINBOX	1
+#define LV_USE_SPINBOX	0
 
-#define LV_USE_SPINNER	1
+#define LV_USE_SPINNER	0
 
-#define LV_USE_SWITCH	 1
+#define LV_USE_SWITCH	 0
 
-#define LV_USE_TABLE	  1
+#define LV_USE_TABLE	  0
 
-#define LV_USE_TABVIEW	1
+#define LV_USE_TABVIEW	0
 
-#define LV_USE_TEXTAREA   1   /**< Requires: lv_label */
+#define LV_USE_TEXTAREA   0   /**< Requires: lv_label */
 #if LV_USE_TEXTAREA != 0
 	#define LV_TEXTAREA_DEF_PWD_SHOW_TIME 1500	/**< [ms] */
 #endif
 
-#define LV_USE_TILEVIEW   1
+#define LV_USE_TILEVIEW   0
 
-#define LV_USE_WIN		1
+#define LV_USE_WIN		0
 
 /*==================
  * THEMES
@@ -829,10 +829,10 @@
 /* Documentation for layouts can be found here: https://docs.lvgl.io/master/layouts/index.html . */
 
 /** A layout similar to Flexbox in CSS. */
-#define LV_USE_FLEX 1
+#define LV_USE_FLEX 0
 
 /** A layout similar to Grid in CSS. */
-#define LV_USE_GRID 1
+#define LV_USE_GRID 0
 
 /*====================
  * 3RD PARTS LIBRARIES
@@ -1121,7 +1121,7 @@
 #define LV_USE_IMGFONT 0
 
 /** 1: Enable an observer pattern implementation */
-#define LV_USE_OBSERVER 1
+#define LV_USE_OBSERVER 0
 
 /** 1: Enable Pinyin input method
  *  - Requires: lv_keyboard */
