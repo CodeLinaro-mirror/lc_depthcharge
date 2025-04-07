2616fcd03d104f7eb8fe25e32a968c22a6cbf810
Yu-Ping Wu <yupingso@chromium.org>
2025-04-07 18:08:20 +0800

    drivers/ec/rts5453: Pass ec_pd_id to board_rts5453_get_image_paths()
    Skywalker's two USB-C ports need different PDC firmware. Therefore,
    pass ec_pd_id to board_rts5453_get_image_paths() to differentiate
    between them.
    
    BUG=b:391519679, b:407463148
    TEST=emerge-skywalker depthcharge
    BRANCH=none
    
    Change-Id: I60bbcb596143c29937711a2af35c7b6ff6b2af8c
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6433633
    Commit-Queue: Yidi Lin <yidilin@google.com>
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6615780
    Tested-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>

--- src/board/brya/pujjocento.c
+++ src/board/brya/pujjocento.c
@@ -14,7 +14,7 @@
 
 /* Override of func in src/drivers/ec/rts5453/rts5453.c */
 void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
-				   struct ec_response_pd_chip_info_v2 *r)
+				   int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
 {
 	*image_path = NULL;
 	*hash_path = NULL;
