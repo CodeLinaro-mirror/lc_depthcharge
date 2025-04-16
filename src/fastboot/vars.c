1a77bcc6cd8998c3a431834927931af922e19569
Eran Mitrani <mitrani@google.com>
2025-04-16 12:49:02 -0700

    fastboot: Add support for "getvar partition-type" command
    This CL adds a variable "partition-type" to getvar. It returns the
    partition type (ext4/vfat/raw).
    
    BUG=b:396352272
    TEST=Tested on Kano:
    fastboot -s tcp:{dut_ip}:5554 getvar partition-type:KERN-C
    fastboot -s tcp:{dut_ip}:5554 getvar all
    
    Change-Id: Icff9a1a75787bd8c9527f3d1effb26ce5eade813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6464072
    Auto-Submit: Eran Mitrani <mitrani@google.com>
    Tested-by: Eran Mitrani <mitrani@google.com>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Commit-Queue: Eran Mitrani <mitrani@google.com>
    (cherry picked from commit 0590d22453942d954ed807b2d7bdd63bbcaf9507)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6559343
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: Yidi Lin <yidilin@google.com>
    Commit-Queue: Yidi Lin <yidilin@google.com>

--- src/fastboot/vars.c
+++ src/fastboot/vars.c
@@ -37,6 +37,7 @@ static fastboot_getvar_info_t fastboot_vars[] = {
 	VAR_NO_ARGS("max-download-size", VAR_DOWNLOAD_SIZE),
 	VAR_NO_ARGS("is-userspace", VAR_IS_USERSPACE),
 	VAR_ARGS("partition-size", ':', VAR_PARTITION_SIZE),
+	VAR_ARGS("partition-type", ':', VAR_PARTITION_TYPE),
 	VAR_NO_ARGS("product", VAR_PRODUCT),
 	VAR_NO_ARGS("secure", VAR_SECURE),
 	VAR_NO_ARGS("slot-count", VAR_SLOT_COUNT),
@@ -114,6 +115,21 @@ void fastboot_cmd_getvar(struct FastbootOps *fb, const char *args)
 	fastboot_fail(fb, "Unknown variable");
 }
 
+/* Helper function to get partition type string based on name */
+static const char *get_partition_type_string(const char *name)
+{
+	if (!strcmp(name, "OEM")) {
+		return "ext4";
+	} else if (!strcmp(name, "EFI-SYSTEM")) {
+		return "vfat";
+	} else if (!strcmp(name, "metadata")) {
+		return "ext4";
+	} else if (!strcmp(name, "userdata")) {
+		return "ext4";
+	}
+	return "raw"; /* All other partitions are "raw" type*/
+}
+
 fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 					 size_t index, char *outbuf,
 					 size_t *outbuf_len)
@@ -180,6 +196,52 @@ fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 
 		used_len += snprintf(outbuf, *outbuf_len, "0x%llx",
 				     GptGetEntrySizeBytes(disk.gpt, part));
+		fastboot_disk_destroy(&disk);
+		break;
+	}
+	case VAR_PARTITION_TYPE: {
+		const char *type_str = "raw";
+		GptEntry *part = NULL;
+		int arg_len = strlen(arg);
+		if (!fastboot_disk_init(&disk))
+			return STATE_DISK_ERROR;
+
+		if (arg != NULL) {
+			if (arg_len == 0) {
+				fastboot_disk_destroy(&disk);
+				return STATE_UNKNOWN_VAR;
+			}
+			type_str = get_partition_type_string(arg);
+			used_len = snprintf(outbuf, *outbuf_len, "%s", type_str);
+
+		} else {
+			char *name;
+
+			if (fastboot_get_number_of_partitions(&disk) <= index) {
+				/* There are no more partitions to get */
+				fastboot_disk_destroy(&disk);
+				return STATE_LAST;
+			}
+
+			part = fastboot_get_partition(&disk, index);
+			if (part == NULL || IsUnusedEntry(part)) {
+				/* Skip invalid/unused entries */
+				fastboot_disk_destroy(&disk);
+				return STATE_TRY_NEXT;
+			}
+
+			name = fastboot_get_entry_name(part);
+			if (name == NULL) {
+				fastboot_disk_destroy(&disk);
+				return STATE_TRY_NEXT;
+			}
+
+			type_str = get_partition_type_string(name);
+
+			used_len = snprintf(outbuf, *outbuf_len, "%s:%s", name, type_str);
+			free(name);
+		}
+
 		fastboot_disk_destroy(&disk);
 		break;
 	}
