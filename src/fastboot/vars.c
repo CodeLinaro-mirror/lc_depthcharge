c8a7fa924a1fd88a4d323e5322d74e18f08b3441
Eran Mitrani <mitrani@google.com>
2025-05-12 13:07:20 -0700

    fastboot: Add slot-retry-count to "getvar all" command
    This CL adds the variable "slot-retry-count" to getvar all.
    
    BUG=b:395616658
    TEST=fastboot -s tcp:{dut_ip}:5554 getvar all
    
    Change-Id: I75d761b1b579dce80594e52dd5ee931dfd5e8f8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6536018
    Tested-by: Eran Mitrani <mitrani@google.com>
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Auto-Submit: Eran Mitrani <mitrani@google.com>
    Commit-Queue: Eran Mitrani <mitrani@google.com>

--- src/fastboot/vars.c
+++ src/fastboot/vars.c
@@ -408,21 +408,32 @@ fastboot_getvar_result_t fastboot_getvar(fastboot_var_t var, const char *arg,
 		if (!fastboot_disk_init(&disk))
 			return STATE_DISK_ERROR;
 
-		if (strlen(arg) != 1 || !isalpha(arg[0])) {
-			fastboot_disk_destroy(&disk);
-			return STATE_UNKNOWN_VAR;
+		if (arg != NULL) {
+			if (strlen(arg) != 1 || !isalpha(arg[0])) {
+				fastboot_disk_destroy(&disk);
+				return STATE_UNKNOWN_VAR; // Invalid slot format
+			}
+			char slot_char_arg = tolower(arg[0]);
+			GptEntry *e = fastboot_get_kernel_for_slot(&disk, slot_char_arg);
+			if (e == NULL) {
+				fastboot_disk_destroy(&disk);
+				return STATE_UNKNOWN_VAR;
+			}
+			used_len = snprintf(outbuf, *outbuf_len, "%d", GetEntryTries(e));
+		} else {
+			// Handling for "getvar all"; arg is NULL, use index.
+			GptEntry *entry_for_index = NULL;
+			char slot_char_for_index = 0;
+			fastboot_getvar_result_t find_slot_state =
+				fastboot_get_kernel_slot_by_index(&disk, index, &entry_for_index,
+								  &slot_char_for_index);
+			if (find_slot_state != STATE_OK) {
+				fastboot_disk_destroy(&disk);
+				return find_slot_state;
+			}
+			used_len = snprintf(outbuf, *outbuf_len, "%c:%d", slot_char_for_index,
+					    GetEntryTries(entry_for_index));
 		}
-
-		char slot = tolower(arg[0]);
-
-		GptEntry *e = fastboot_get_kernel_for_slot(&disk, slot);
-		if (e == NULL) {
-			fastboot_disk_destroy(&disk);
-			return STATE_UNKNOWN_VAR;
-		}
-
-		used_len = snprintf(outbuf, *outbuf_len, "%d", GetEntryTries(e));
-
 		fastboot_disk_destroy(&disk);
 		break;
 	}
