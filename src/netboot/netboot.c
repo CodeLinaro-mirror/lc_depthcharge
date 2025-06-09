a5d2e29498e3b241c370023a2f6246ff4cb79793
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-06-09 11:17:11 +0800

    netboot: Add sustain battery function in netboot download
    Add sustain battery function in netboot download
    
    BUG=b:423484218
    TEST=FW_NAME=kanix emerge-rex depthcharge coreboot
    chromeos-bootimage
    BRANCH=None
    Signed-off-by: Yu-an Chen <yu-an.chen@quanta.corp-partner.google.com>
    
    Change-Id: I14395eba6c8b412da662b4b7a8e3d08d26f9aff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6629871
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Commit-Queue: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6627457
    Reviewed-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Tested-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Commit-Queue: David Wu <david_wu@quanta.corp-partner.google.com>
    (cherry picked from commit 5d4b0f4b58c90d07156de092bcfbb25a1b60e9b6)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6801310
    Reviewed-by: caveh jalali <caveh@chromium.org>

--- src/netboot/netboot.c
+++ src/netboot/netboot.c
@@ -22,6 +22,7 @@
 #include "base/late_init_funcs.h"
 #include "base/timestamp.h"
 #include "debug/cli/common.h"
+#include "drivers/ec/cros/ec.h"
 #include "drivers/input/input.h"
 #include "drivers/net/net.h"
 #include "drivers/power/power.h"
@@ -257,6 +258,10 @@ int netboot_entry(void)
 	if (netboot_params_read(&tftp_ip, cmd_line, sizeof(cmd_line),
 				&bootfile, &argsfile))
 		printf("ERROR: Failed to read netboot parameters from flash\n");
+
+	// sustain battery at 40%~50% during netboot download
+	if (CONFIG(DRIVER_EC_CROS))
+		cros_ec_battery_sustain(40, 50);
 
 	netboot(tftp_ip, bootfile, argsfile, cmd_line, NULL);
 
