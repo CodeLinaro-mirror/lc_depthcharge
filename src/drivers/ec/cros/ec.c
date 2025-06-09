d711380667c0f3cd4dfead82e42d5423fb93522e
Yu-An <yu-an.chen@quanta.corp-partner.google.com>
2025-06-09 11:17:11 +0800

    netboot: Add sustain battery function in netboot download
    Add sustain battery function in netboot download
    
    BUG=b:423484218
    TEST=FW_NAME=kanix emerge-rex depthcharge coreboot
    chromeos-bootimage
    BRANCH=None
    Signed-off-by: Yu-an Chen <yu-an.chen@quanta.corp-partner.google.com>
    
    Change-Id: I14395eba6c8b412da662b4b7a8e3d08d26f9aff8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6629871
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Commit-Queue: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Tested-by: Yu-An Chen <yu-an.chen@quanta.corp-partner.google.com>
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    (cherry picked from commit 211287e066822f071268c535e3f365bf5147d90a)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6966277
    Tested-by: Eren Peng <peng.eren@inventec.corp-partner.google.com>
    Reviewed-by: Minho Park <minhopark@google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/drivers/ec/cros/ec.c
+++ src/drivers/ec/cros/ec.c
@@ -1052,6 +1052,32 @@ static vb2_error_t vboot_battery_cutoff(VbootEcOps *vbec)
 	return VB2_SUCCESS;
 }
 
+int cros_ec_battery_sustain(unsigned char lower, unsigned char upper)
+{
+	struct ec_params_charge_control p;
+	int version;
+
+	p.cmd = EC_CHARGE_CONTROL_CMD_SET;
+	p.mode = CHARGE_CONTROL_NORMAL;
+	p.sustain_soc.lower = lower;
+	p.sustain_soc.upper = upper;
+
+	if (cros_ec_cmd_version_supported(EC_CMD_CHARGE_CONTROL, 3))
+		version = 3;
+	else
+		version = 2;
+
+	int rv = ec_command(cros_ec_get(), EC_CMD_CHARGE_CONTROL, version,
+			&p, sizeof(p), NULL, 0);
+
+	if (rv < 0)
+		printf("error, set battery sustainer fail!\n");
+
+	printf("sustain battery at %d~%d\n", lower, upper);
+
+	return rv;
+}
+
 int cros_ec_set_motion_sense_activity(uint32_t activity, uint32_t value)
 {
 	struct ec_params_motion_sense params;
@@ -1337,7 +1363,7 @@ int cros_ec_pd_chip_info(int port, int renew,
 	 * if not use EC_CMD_PD_CHIP_INFO instead.
 	 */
 
-	if (cmd_version_supported(cros_ec_get(), EC_CMD_PD_CHIP_INFO, 2))
+	if (cros_ec_cmd_version_supported(EC_CMD_PD_CHIP_INFO, 2))
 		ret = ec_command(cros_ec_get(), EC_CMD_PD_CHIP_INFO, 2,
 				&p, sizeof(p), r, sizeof(*r));
 	else
@@ -1520,7 +1546,7 @@ void cros_ec_probe_aux_fw_chips(void)
 		 * Check if EC_CMD_PD_CHIP_INFO(v2) is supported,
 		 * if not use EC_CMD_PD_CHIP_INFO instead.
 		 */
-		if (cmd_version_supported(cros_ec, EC_CMD_PD_CHIP_INFO, 2))
+		if (cros_ec_cmd_version_supported( EC_CMD_PD_CHIP_INFO, 2))
 			ret = ec_command(cros_ec, EC_CMD_PD_CHIP_INFO, 2,
 					&pd_chip_p, sizeof(pd_chip_p),
 					&pd_chip_r, sizeof(pd_chip_r));
