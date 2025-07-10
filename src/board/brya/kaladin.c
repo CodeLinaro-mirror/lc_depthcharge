cdc504603af3304939a259c439c80a0fe97660be
peng.eren <peng.eren@inventec.corp-partner.google.com>
2025-07-10 10:53:42 +0800

    kaladin: Add FW config probe for NVMe
    Add FW config probe for NVMe
    
    BUG=430725546
    TEST=Build and flash to DUT, verify DUT can boot from NVMe
    
    Change-Id: I18531ef99b26e7a7153cc3ded12f2488e44388af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6720944
    Tested-by: Eren Peng <peng.eren@inventec.corp-partner.google.com>
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    (cherry picked from commit 2237733bd30084b6d994894c5aec3923db584b54)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6800907
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- src/board/brya/kaladin.c
+++ src/board/brya/kaladin.c
@@ -54,6 +54,10 @@ static const struct storage_config storage_configs[] = {
 		.pci_dev = PCH_DEV_UFS1,
 		.fw_config = FW_CONFIG(STORAGE, STORAGE_UFS)
 	},
+	{ 	.media = STORAGE_NVME,
+		.pci_dev = PCH_DEV_PCIE10,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_NVME)
+	},
 	{
 		.media = STORAGE_EMMC,
 		.pci_dev = PCH_DEV_EMMC,
@@ -67,6 +71,10 @@ static const struct storage_config storage_configs[] = {
 	{
 		.media = STORAGE_UFS,
 		.pci_dev = PCH_DEV_UFS1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+	{ 	.media = STORAGE_NVME,
+		.pci_dev = PCH_DEV_PCIE10,
 		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
 	},
 };
