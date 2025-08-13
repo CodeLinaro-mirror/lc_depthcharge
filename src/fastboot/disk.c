coreboot/depthcharge

fastboot/disk: Add offset argument to fastboot_write Android Bringup: See http://go/android-fw-sync
Add offset argument to fastboot_write, to allow writes starting at
different place than the beginning of the partition.

BRANCH=firmware-android-15949.B
BUG=b:374092472
TEST=None

(cherry picked from commit cf4c6bc9c204999a232b92d6ad45a4c0e90f583e)

Change-Id: I71df3e83baf46b589a78c87e5105b3556fc47336
Original-Signed-off-by: Tomasz Michalec <tmichalec@google.com>
Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6084719
Original-Reviewed-by: Jakub Czapiga <czapiga@google.com>
GitOrigin-RevId: cf4c6bc9c204999a232b92d6ad45a4c0e90f583e
Cr-Build-Id: 8725645506990902993
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8725645506990902993
Copybot-Job-Name: android-main-fw-corsola-depthcharge-copybot-downstream
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6178081
Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
Bot-Commit: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

src/fastboot/disk.c
@@ -188,7 +188,7 @@ bool fastboot_read(struct fastboot_disk *disk,
 
 void fastboot_write(struct FastbootOps *fb, struct fastboot_disk *disk,
 		    const char *partition_name, size_t name_len, void *data,
-		    size_t data_len)
+		    size_t data_len, size_t offset)
 {
 	GptEntry *e = fastboot_find_partition(disk, partition_name, name_len);
 	if (!e) {
@@ -196,30 +196,52 @@ void fastboot_write(struct FastbootOps *fb, struct fastboot_disk *disk,
 		return;
 	}
 
+	if (is_sparse_image(data)) {
+		if (offset) {
+			FB_FAIL_AND_DEBUG(fb, "Non-zero offset for sparse image write");
+			return;
+		}
+
+		FB_DEBUG("Writing sparse image to LBA %llu to %llu\n",
+			 e->starting_lba, e->ending_lba);
+		if (!write_sparse_image(fb, disk, e, data, data_len))
+			fastboot_succeed(fb);
+
+		return;
+	}
+
+	if (offset % disk->disk->block_size) {
+		fastboot_fail(fb, "Offset %zu not block size aligned %u\n", offset,
+			      disk->disk->block_size);
+		return;
+	}
+
+	if (data_len % disk->disk->block_size) {
+		fastboot_fail(fb, "Buffer size %zu not block size aligned %u\n", data_len,
+			      disk->disk->block_size);
+		return;
+	}
+
 	uint64_t space = GptGetEntrySizeLba(e);
-	uint64_t data_blocks = div_round_up(data_len, disk->disk->block_size);
-	if (data_blocks > space) {
+	uint64_t data_blocks = data_len / disk->disk->block_size;
+	uint64_t data_blocks_offset = offset / disk->disk->block_size;
+	if (data_blocks + data_blocks_offset > space) {
 		fastboot_fail(fb, "Image is too big");
 		return;
 	}
 
-	if (is_sparse_image(data)) {
-		FB_DEBUG("Writing sparse image to LBA %llu to %llu\n",
-			 e->starting_lba, e->ending_lba);
-		if (write_sparse_image(fb, disk, e, data, data_len))
-			return;
-	} else {
-		FB_DEBUG("Writing LBA %llu to %llu, num blocks = %llu, data "
-			 "len = %zu, block size = %u\n",
-			 e->starting_lba, e->starting_lba + data_blocks,
-			 data_blocks, data_len, disk->disk->block_size);
-		lba_t blocks_written = disk->disk->ops.write(
-			&disk->disk->ops, e->starting_lba, data_blocks, data);
-		if (blocks_written != data_blocks) {
-			fastboot_fail(fb, "Failed to write");
-			return;
-		}
+	FB_DEBUG("Writing LBA %llu to %llu, num blocks = %llu, data "
+		 "len = %zu, block size = %u\n",
+		 e->starting_lba + data_blocks_offset,
+		 e->starting_lba + data_blocks_offset + data_blocks,
+		 data_blocks, data_len, disk->disk->block_size);
+	lba_t blocks_written = disk->disk->ops.write(
+		&disk->disk->ops, e->starting_lba + data_blocks_offset, data_blocks, data);
+	if (blocks_written != data_blocks) {
+		fastboot_fail(fb, "Failed to write");
+		return;
 	}
+
 	fastboot_succeed(fb);
 	return;
 }

CodeLinaro-mirror/lc_depthcharge

BRANCH=main
BUG=b:358095850
TEST=fastboot flash is still working as expected
(cherry picked from commit 53a808130d928057a8bdbda4cd6ee28f8caa6b97)
Change-Id: Ia79a70ec4ac1d1f05624d85e885b76af67c5f33e
Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6198624
Original-Reviewed-by: Julius Werner <jwerner@chromium.org>
GitOrigin-RevId: 53a808130d928057a8bdbda4cd6ee28f8caa6b97
Cr-Build-Id: 8707186380091747489
Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8707186380091747489
Copybot-Job-Name: android-main-depthcharge-copybot-downstream
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6826680
Commit-Queue: Tomasz Michalec <tmichalec@google.com>
Reviewed-by: Konrad Adamczyk <konrada@google.com>
Tested-by: Tomasz Michalec <tmichalec@google.com>

src/fastboot/disk.c
@@ -220,17 +220,18 @@ void fastboot_write_raw(struct FastbootOps *fb, struct fastboot_disk *disk,
 }
 
 void fastboot_write(struct FastbootOps *fb, struct fastboot_disk *disk,
-		    const char *partition_name, void *data, size_t data_len, size_t offset)
+		    const char *partition_name, const uint64_t blocks_offset,
+		    void *data, size_t data_len)
 {
 	GptEntry *e = fastboot_find_partition(disk, partition_name);
 	if (!e) {
-		fastboot_fail(fb, "Could not find partition");
+		fastboot_fail(fb, "Could not find partition \"%s\"\n", partition_name);
 		return;
 	}
 
 	if (is_sparse_image(data)) {
-		if (offset) {
-			FB_FAIL_AND_DEBUG(fb, "Non-zero offset for sparse image write");
+		if (blocks_offset) {
+			fastboot_fail(fb, "Non-zero offset for sparse image write");
 			return;
 		}
 
@@ -242,22 +243,33 @@ void fastboot_write(struct FastbootOps *fb, struct fastboot_disk *disk,
 		return;
 	}
 
-	if (offset % disk->disk->block_size) {
-		fastboot_fail(fb, "Offset %zu not block size aligned %u\n", offset,
+	if (data_len % disk->disk->block_size) {
+		fastboot_fail(fb, "Buffer size %zu not aligned to block size of %u\n", data_len,
 			      disk->disk->block_size);
 		return;
 	}
 
-	uint64_t space = GptGetEntrySizeLba(e);
-	uint64_t blocks_offset = offset / disk->disk->block_size;
-	if (blocks_offset >= space) {
-		fastboot_fail(fb, "Offset larger than partition size (%llu >= %llu)\n",
-			      blocks_offset, space);
+	const uint64_t space = GptGetEntrySizeLba(e);
+	const uint64_t data_blocks = data_len / disk->disk->block_size;
+	if (blocks_offset > space || data_blocks > space - blocks_offset) {
+		fastboot_fail(fb, "Image is too big");
 		return;
 	}
 
-	fastboot_write_raw(fb, disk, e->starting_lba + blocks_offset, space - blocks_offset,
-			   data, data_len);
+	FB_DEBUG("Writing LBA %llu to %llu, num blocks = %llu, data "
+		 "len = %zu, block size = %u\n",
+		 e->starting_lba + blocks_offset,
+		 e->starting_lba + blocks_offset + data_blocks,
+		 data_blocks, data_len, disk->disk->block_size);
+	const lba_t blocks_written = disk->disk->ops.write(
+		&disk->disk->ops, e->starting_lba + blocks_offset, data_blocks, data);
+	if (blocks_written != data_blocks) {
+		fastboot_fail(fb, "Failed to write");
+		return;
+	}
+
+	fastboot_succeed(fb);
+	return;
 }
 
 void fastboot_erase(struct FastbootOps *fb, struct fastboot_disk *disk,