coreboot/depthcharge

vboot: Removed usused code in boot policy
Simplify code. Setting current policy is no long needed after removing
old boards which used KERNEL_IMAGE_BOOTIMG type (CL:6172034).

BUG=none
TEST=With consecutive CLs applied build FW and boot android
BRANCH=main

Change-Id: I15a916a42781184440dab3267b15c99a1c590a29
Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6281286
Reviewed-by: Julius Werner <jwerner@chromium.org>
Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

src/vboot/boot_info.h
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2015 Google LLC
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __VBOOT_BOOT_INFO_H__
+#define __VBOOT_BOOT_INFO_H__
+
+#include <vb2_api.h>
+
+#include "vboot/boot.h"
+
+/*
+ * Flags field in vboot kernel preamble is defined as:
+ * [31:2] - Reserved (for future use)
+ * [1:0]  - Image type (0x0 = CrOS, 0x1 = Bootimg, 0x2 = Multiboot)
+ */
+#define KERNEL_IMG_TYPE_MASK	(0x3)
+#define KERNEL_IMG_TYPE_SHIFT	(0x0)
+
+#define GET_KERNEL_IMG_TYPE(x)					\
+	(((x) >> KERNEL_IMG_TYPE_SHIFT) & KERNEL_IMG_TYPE_MASK)
+
+typedef enum {
+	KERNEL_IMAGE_CROS = 0 << KERNEL_IMG_TYPE_SHIFT,
+	KERNEL_IMAGE_BOOTIMG = 1 << KERNEL_IMG_TYPE_SHIFT,
+	KERNEL_IMAGE_MULTIBOOT = 2 << KERNEL_IMG_TYPE_SHIFT,
+} kernel_img_type_t;
+
+enum {
+	CmdLineSize = 4096,
+	CrosParamSize = 4096,
+};
+
+/*
+ * Depending upon kernel image type, this function selects the appropriate function
+ * to fill up the boot_info structure.
+ *
+ * Returns 0 on success, -1 on error. Error can occur if the image type handler
+ * is not able to setup the boot info structure for any reason.
+ */
+int fill_boot_info(struct boot_info *bi, struct vb2_kernel_params *kparams);
+
+#endif /* __VBOOT_BOOT_INFO_H__ */

CodeLinaro-mirror/lc_depthcharge

(cherry picked from commit 22286235fa2cb88c1560333890229e7c61c5521a)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6983452
Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
Reviewed-by: Konrad Adamczyk <konrada@google.com>

src/vboot/boot_info.h
@@ -1,0 +1,56 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __VBOOT_BOOT_INFO_H__
+#define __VBOOT_BOOT_INFO_H__
+
+#include <vboot_api.h>
+
+#include "vboot/boot.h"
+
+/*
+ * Flags field in vboot kernel preamble is defined as:
+ * [31:2] - Reserved (for future use)
+ * [1:0]  - Image type (0x0 = CrOS, 0x1 = Bootimg, 0x2 = Multiboot)
+ */
+#define KERNEL_IMG_TYPE_MASK	(0x3)
+#define KERNEL_IMG_TYPE_SHIFT	(0x0)
+
+#define GET_KERNEL_IMG_TYPE(x)					\
+	(((x) >> KERNEL_IMG_TYPE_SHIFT) & KERNEL_IMG_TYPE_MASK)
+
+typedef enum {
+	KERNEL_IMAGE_CROS = 0 << KERNEL_IMG_TYPE_SHIFT,
+	KERNEL_IMAGE_BOOTIMG = 1 << KERNEL_IMG_TYPE_SHIFT,
+	KERNEL_IMAGE_MULTIBOOT = 2 << KERNEL_IMG_TYPE_SHIFT,
+} kernel_img_type_t;
+
+enum {
+	CmdLineSize = 4096,
+	CrosParamSize = 4096,
+};
+
+/*
+ * Depending upon kernel image type, this function selects the appropriate function
+ * to fill up the boot_info structure.
+ *
+ * Returns 0 on success, -1 on error. Error can occur if the image type handler
+ * is not able to setup the boot info structure for any reason.
+ */
+int fill_boot_info(struct boot_info *bi, VbSelectAndLoadKernelParams *kparams);
+
+#endif /* __VBOOT_BOOT_INFO_H__ */
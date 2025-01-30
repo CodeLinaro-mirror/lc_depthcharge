ffcea11e6955c5fbc9bc54699c21ce3120d2ac55
Grzegorz Bernacki <bernacki@google.com>
2025-01-30 13:23:49 +0000

    vboot: Removed usused code in boot policy
    Simplify code. Setting current policy is no long needed after removing
    old boards which used KERNEL_IMAGE_BOOTIMG type (CL:6172034).
    
    BUG=none
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    Change-Id: I15a916a42781184440dab3267b15c99a1c590a29
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6281286
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    (cherry picked from commit 22286235fa2cb88c1560333890229e7c61c5521a)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6983452
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Konrad Adamczyk <konrada@google.com>

--- src/board/eve/board.c
+++ src/board/eve/board.c
@@ -41,7 +41,7 @@
 #include "drivers/storage/sdhci.h"
 #include "drivers/tpm/google/i2c.h"
 #include "drivers/tpm/tpm.h"
-#include "vboot/boot_policy.h"
+#include "vboot/boot_info.h"
 #include "vboot/stages.h"
 #include "vboot/util/commonparams.h"
 #include "vboot/util/flag.h"
