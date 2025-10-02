coreboot/depthcharge

vboot: Use Android BCB to select boot mode
Use Android boot control block (BCB) to select Android normal or
recovery mode. Selected mode is announced to vboot by new
vb2ex_get_android_bootmode callback.

If BCB requests it, fastboot may be started if
VB2_GBB_FLAG_FORCE_UNLOCK_FASTBOOT flag is set.

BRANCH=main
BUG=None
TEST=Reboot Android to recovery using "reboot recovery"
     Reboot Android to FW fastboot using "reboot bootloader"
     Boot Android in normal mode

Cq-Depend: chromium:6574535
Change-Id: I7d337d3714d087d85aae3b04fa90ed482d3b610f
Signed-off-by: Tomasz Michalec <tmichalec@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6574541
Reviewed-by: Julius Werner <jwerner@chromium.org>

src/vboot/callbacks/misc.c
@@ -20,7 +20,10 @@
 #include <lzma.h>
 #include <vb2_api.h>
 
+#include "base/android_misc.h"
+#include "debug/dev.h"
 #include "drivers/flash/flash.h"
+#include "fastboot/fastboot.h"
 #include "vboot/nvdata.h"
 #include "vboot/secdata_tpm.h"
 
@@ -96,4 +99,44 @@ vb2_error_t vb2ex_commit_data(struct vb2_context *ctx)
 	}
 
 	return rv;
+}
+
+vb2_error_t vb2ex_get_android_bootmode(struct vb2_context *ctx,
+				       vb2ex_disk_handle_t disk,
+				       GptData *gpt,
+				       enum vb2_android_bootmode *bootmode)
+{
+	enum android_misc_bcb_command cmd;
+	BlockDev *bdev = (BlockDev *)disk;
+
+	/* Ignore misc partition on external disks */
+	if (bdev->removable) {
+		*bootmode = VB2_ANDROID_NORMAL_BOOT;
+		return VB2_SUCCESS;
+	}
+
+	cmd = android_misc_get_bcb_command(bdev, gpt);
+	switch (cmd) {
+	case MISC_BCB_NORMAL_BOOT:
+	/* For unknown command, just continue with normal boot */
+	case MISC_BCB_UNKNOWN:
+	/*
+	 * If there is problem with misc partition, just allow normal boot and let OS to
+	 * figure it out if it boot
+	 */
+	case MISC_BCB_DISK_ERROR:
+		*bootmode = VB2_ANDROID_NORMAL_BOOT;
+		break;
+	case MISC_BCB_RECOVERY_BOOT:
+		*bootmode = VB2_ANDROID_RECOVERY_BOOT;
+		break;
+	case MISC_BCB_BOOTLOADER_BOOT:
+		*bootmode = VB2_ANDROID_NORMAL_BOOT;
+		/* If GBB flag is set, start fastboot */
+		if (vb2api_gbb_get_flags(ctx) & VB2_GBB_FLAG_FORCE_UNLOCK_FASTBOOT)
+			fastboot();
+		break;
+	}
+
+	return VB2_SUCCESS;
 }

CodeLinaro-mirror/lc_depthcharge

(cherry picked from commit 3e905254cb0e1693a10d8e14a6fe515f473d8bba)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6984362
Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
Reviewed-by: Konrad Adamczyk <konrada@google.com>
Tested-by: Grzegorz Bernacki <bernacki@google.com>

src/vboot/callbacks/misc.c
@@ -20,7 +20,10 @@
 #include <lzma.h>
 #include <vb2_api.h>
 
+#include "base/android_misc.h"
+#include "debug/dev.h"
 #include "drivers/flash/flash.h"
+#include "fastboot/fastboot.h"
 #include "vboot/nvdata.h"
 #include "vboot/secdata_tpm.h"
 
@@ -100,4 +103,44 @@ vb2_error_t vb2ex_commit_data(struct vb2_context *ctx)
 	}
 
 	return rv;
+}
+
+vb2_error_t vb2ex_get_android_bootmode(struct vb2_context *ctx,
+				       VbExDiskHandle_t disk,
+				       GptData *gpt,
+				       enum vb2_android_bootmode *bootmode)
+{
+	enum android_misc_bcb_command cmd;
+	BlockDev *bdev = (BlockDev *)disk;
+
+	/* Ignore misc partition on external disks */
+	if (bdev->removable) {
+		*bootmode = VB2_ANDROID_NORMAL_BOOT;
+		return VB2_SUCCESS;
+	}
+
+	cmd = android_misc_get_bcb_command(bdev, gpt);
+	switch (cmd) {
+	case MISC_BCB_NORMAL_BOOT:
+	/* For unknown command, just continue with normal boot */
+	case MISC_BCB_UNKNOWN:
+	/*
+	 * If there is problem with misc partition, just allow normal boot and let OS to
+	 * figure it out if it boot
+	 */
+	case MISC_BCB_DISK_ERROR:
+		*bootmode = VB2_ANDROID_NORMAL_BOOT;
+		break;
+	case MISC_BCB_RECOVERY_BOOT:
+		*bootmode = VB2_ANDROID_RECOVERY_BOOT;
+		break;
+	case MISC_BCB_BOOTLOADER_BOOT:
+		*bootmode = VB2_ANDROID_NORMAL_BOOT;
+		/* If GBB flag is set, start fastboot */
+		if (vb2api_gbb_get_flags(ctx) & VB2_GBB_FLAG_FORCE_UNLOCK_FASTBOOT)
+			fastboot();
+		break;
+	}
+
+	return VB2_SUCCESS;
 }