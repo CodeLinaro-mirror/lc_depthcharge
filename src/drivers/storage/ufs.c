coreboot/depthcharge

fastboot: add an ability to read UFS descriptors
BUG=b:424375985
TEST=fastboot -s tcp:<IP>:5554 oem read-ufs-descriptor:7,0
TEST=fastboot -s tcp:<IP>:5554 get_staged /tmp/x
TEST=hexdump /tmp/x
BRANCH=none

Change-Id: Id2cce3e47e13d9f3bb131b43698a39a83a459bb9
Signed-off-by: Daniil Lunev <dlunev@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6647368
Reviewed-by: Julius Werner <jwerner@chromium.org>

src/drivers/storage/ufs.c
@@ -822,13 +822,16 @@ static int ufs_set_refclkfreq(UfsCtlr *ufs)
 }
 
 // Read a UFS descriptor
-static int ufs_read_descriptor(UfsCtlr *ufs, uint8_t idn, uint8_t idx,
-			       uint8_t *buf, uint8_t len, uint8_t *resp_len)
+int ufs_read_descriptor(UfsCtlr *ufs, uint8_t idn, uint8_t idx,
+			uint8_t *buf, uint64_t len, uint8_t *resp_len)
 {
+	uint8_t rl = UFS_DESCRIPTOR_MAX_SIZE;
+	if (len < UFS_DESCRIPTOR_MAX_SIZE)
+		rl = (uint8_t)(len & 0xFF);
 	UfsQryReq req = {
 		.idn = idn,
 		.idx = idx,
-		.resp_data_len = len,
+		.resp_data_len = rl,
 		.resp_data_buf = buf,
 	};
 	int rc;
@@ -1225,6 +1228,24 @@ static int ufs_add_device(UfsCtlr *ufs, uint32_t lun)
 	ufs->ufs_dev[lun] = ufs_dev;
 
 	return 0;
+}
+
+UfsCtlr *ufs_get_ctlr(void)
+{
+	struct list_node *ctrlrs = &fixed_block_dev_controllers;
+	BlockDevCtrlr *ctrlr;
+
+	list_for_each(ctrlr, *ctrlrs, list_node) {
+		if (ctrlr->type != BLOCK_CTRL_UFS)
+			continue;
+		if (ctrlr->ops.update && ctrlr->need_update &&
+		    ctrlr->ops.update(&ctrlr->ops)) {
+			printf("Updating storage controller failed.\n");
+			return NULL;
+		}
+		return container_of(ctrlr, UfsCtlr, bctlr);
+	}
+	return NULL;
 }
 
 int ufs_update(BlockDevCtrlrOps *bdev_ops)

CodeLinaro-mirror/lc_depthcharge

(cherry picked from commit c93ae5d69b06554b6889411cc44a0863e36be644)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6984349
Reviewed-by: Konrad Adamczyk <konrada@google.com>
Tested-by: Grzegorz Bernacki <bernacki@google.com>
Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

src/drivers/storage/ufs.c
@@ -789,13 +789,16 @@ static int ufs_set_refclkfreq(UfsCtlr *ufs)
 }
 
 // Read a UFS descriptor
-static int ufs_read_descriptor(UfsCtlr *ufs, uint8_t idn, uint8_t idx,
-			       uint8_t *buf, uint8_t len, uint8_t *resp_len)
+int ufs_read_descriptor(UfsCtlr *ufs, uint8_t idn, uint8_t idx,
+			uint8_t *buf, uint64_t len, uint8_t *resp_len)
 {
+	uint8_t rl = UFS_DESCRIPTOR_MAX_SIZE;
+	if (len < UFS_DESCRIPTOR_MAX_SIZE)
+		rl = (uint8_t)(len & 0xFF);
 	UfsQryReq req = {
 		.idn = idn,
 		.idx = idx,
-		.resp_data_len = len,
+		.resp_data_len = rl,
 		.resp_data_buf = buf,
 	};
 	int rc;
@@ -1179,6 +1182,24 @@ static int ufs_add_device(UfsCtlr *ufs, uint32_t lun)
 	ufs->ufs_dev[lun] = ufs_dev;
 
 	return 0;
+}
+
+UfsCtlr *ufs_get_ctlr(void)
+{
+	struct ListNode *ctrlrs = &fixed_block_dev_controllers;
+	BlockDevCtrlr *ctrlr;
+
+	list_for_each(ctrlr, *ctrlrs, list_node) {
+		if (ctrlr->type != BLOCK_CTRL_UFS)
+			continue;
+		if (ctrlr->ops.update && ctrlr->need_update &&
+		    ctrlr->ops.update(&ctrlr->ops)) {
+			printf("Updating storage controller failed.\n");
+			return NULL;
+		}
+		return container_of(ctrlr, UfsCtlr, bctlr);
+	}
+	return NULL;
 }
 
 int ufs_update(BlockDevCtrlrOps *bdev_ops)