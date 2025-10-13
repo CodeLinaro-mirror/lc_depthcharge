6d3ac564f7002c367fbc15ff1bf6193131bb3e46
Ivan Chen <yulunchen@google.com>
2025-10-13 07:01:50 +0000

    drivers/ec: Add board_rts545x_register function
    Add override function board_rts545x_register to unify the function
    for adding pid in board level if the pid in preflash PDC FW is not
    aligned to the allocated one.
    
    BRANCH=none
    BUG=b:423796270
    TEST=CQ
    
    Signed-off-by: Ivan Chen <yulunchen@google.com>
    Change-Id: I1be8968172447c1241332aa3a32743b76ce3a194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7031302
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    (cherry picked from commit 9d325035cc4c395190619f38257adf3a8f4a3fcb)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7380089
    Tested-by: Yu-Ping Wu <yupingso@chromium.org>
    Auto-Submit: Yu-Ping Wu <yupingso@chromium.org>

--- src/board/skywalker/board.c
+++ src/board/skywalker/board.c
@@ -351,7 +351,7 @@ static void sound_setup(void)
 		printf("no amps found\n");
 }
 
-static void rts545x_register(void)
+void board_rts545x_register(void)
 {
 	/*
 	 * Skywalker's PDC FW 1.61.1 contains the wrong PID 0x5065.
@@ -531,9 +531,6 @@ static int board_setup(void)
 	} else {
 		printf("[%s] no display init required!\n", __func__);
 	}
-
-	if (CONFIG(DRIVER_EC_RTS5453))
-		rts545x_register();
 
 	return 0;
 }
