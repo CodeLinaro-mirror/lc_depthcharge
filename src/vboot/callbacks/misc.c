coreboot/depthcharge

vboot: Rename vb2ex_get_android_bootmode
Rename `vb2ex_get_android_bootmode` to
`vb2ex_handle_android_misc_partition` to more accurately reflect its
purpose, which now includes handling more than just the boot mode.

BRANCH=none
BUG=b:438666196
TEST=emerge-rauru libpayload depthcharge

Cq-Depend: chromium:6979705
Change-Id: I24fd849607abd91f85d7d279071a0578164f2036
Signed-off-by: Yidi Lin <yidilin@google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6979175
Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
Reviewed-by: Julius Werner <jwerner@chromium.org>

src/vboot/callbacks/misc.c
@@ -102,10 +102,10 @@ vb2_error_t vb2ex_commit_data(struct vb2_context *ctx)
 	return rv;
 }
 
-vb2_error_t vb2ex_get_android_bootmode(struct vb2_context *ctx,
-				       vb2ex_disk_handle_t disk,
-				       GptData *gpt,
-				       enum vb2_android_bootmode *bootmode)
+vb2_error_t vb2ex_handle_android_misc_partition(struct vb2_context *ctx,
+						vb2ex_disk_handle_t disk,
+						GptData *gpt,
+						enum vb2_android_bootmode *bootmode)
 {
 	enum android_misc_bcb_command cmd;
 	BlockDev *bdev = (BlockDev *)disk;

CodeLinaro-mirror/lc_depthcharge

Cq-Depend: chromium:7105907
(cherry picked from commit 282ab022f12a206834200899e621155a9ea945ba)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7128861
Reviewed-by: Konrad Adamczyk <konrada@google.com>
Tested-by: Grzegorz Bernacki <bernacki@google.com>
Commit-Queue: Grzegorz Bernacki <bernacki@google.com>

src/vboot/callbacks/misc.c
@@ -106,10 +106,10 @@ vb2_error_t vb2ex_commit_data(struct vb2_context *ctx)
 	return rv;
 }
 
-vb2_error_t vb2ex_get_android_bootmode(struct vb2_context *ctx,
-				       VbExDiskHandle_t disk,
-				       GptData *gpt,
-				       enum vb2_android_bootmode *bootmode)
+vb2_error_t vb2ex_handle_android_misc_partition(struct vb2_context *ctx,
+						VbExDiskHandle_t disk,
+						GptData *gpt,
+						enum vb2_android_bootmode *bootmode)
 {
 	enum android_misc_bcb_command cmd;
 	BlockDev *bdev = (BlockDev *)disk;