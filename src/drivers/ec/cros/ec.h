781089f18e87589f0c7f9e70ddaef22339832a1c
Tristan Honscheid <honscheid@google.com>
2026-05-28 15:58:07 -0600

    ec: Add cros_ec_ap_print()
    Add a variant of the existing cros_ec_print() function that also prints
    a copy of the message to the AP console.
    
    This is intended to capture AP FW log messages that are useful for
    debugging/bug reconstruction but are missing when running non-serial AP
    FW images in the lab, by sending a copy to the EC as well.
    
    This function should not be used in the normal boot path because it
    calls an EC host command and will hurt boot time metrics. Use only in
    error pathways or EC/Aux firmware updates.
    
    BRANCH=None
    BUG=b:517245651
    TEST=emerge-ocelot depthcharge
    
    Change-Id: I1c795d08f76d5de158551167661b7048f7887d06
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7881830
    Reviewed-by: Pranava Y N <pranavayn@google.com>
    (cherry picked from commit f5a8dc1dc6cec47e4281bcc53e5c9f553a8f573d)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/8263818
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Owners-Override: Jeremy Bettis <jbettis@chromium.org>

--- src/drivers/ec/cros/ec.h
+++ src/drivers/ec/cros/ec.h
@@ -448,4 +448,26 @@ int cros_ec_get_usb_pd_mux_info(int port, uint8_t *mux_state);
  */
 int cros_ec_get_usb_pd_control(int port, int *ufp, int *dbg_acc);
 
+/**
+ * Print message on the EC console.
+ *
+ * @param fmt		Format string
+ * @param ...		printf like arguments for format string
+ * @return 0 if ok, negative on error
+ */
+int cros_ec_print(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+
+/**
+ * Print message on the EC and AP consoles
+ *
+ * This triggers an EC host command call for each invocation. Do not use during
+ * the normal boot flow to avoid boot time metric impacts. (Use only in error
+ * pathways or exceptional boot flows like ESS or aux FW sync)
+ *
+ * @param fmt		Format string
+ * @param ...		printf like arguments for format string
+ * @return 0 if ok, negative on error
+ */
+int cros_ec_ap_print(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+
 #endif /* __DRIVERS_EC_CROS_EC_H__ */
